fbc05caf36effc405f73e66cd4e7edc1e2767b30
vilonovlev@gmail.com <vilonovlev@gmail.com>
2023-10-17 15:50:14 +0300

    HW2

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,57 @@
+package org.example;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ImplFullStack dev = new ImplFullStack();
+        ImplBackEnd backEnd = new ImplBackEnd();
+        
+        List<BackEnder> list = List.of(dev,backEnd);
+        list.stream().forEach(x -> x.createServer());
+
+        Developer devFront = new FrontEnder(){
+            @Override
+            public void createFront() {
+                System.out.println("Create Frontend");
+            }
+        };
+        ((FrontEnder)devFront).createFront();
+
+    }
+}
+
+interface BackEnder extends Developer{
+        void createServer();
+}
+
+interface FrontEnder extends Developer{
+    void createFront();
+}
+
+interface FullStack extends BackEnder, FrontEnder {
+}
+
+class ImplFullStack implements FullStack{
+
+    @Override
+    public void createServer() {
+        System.out.println("Server start!");
+    }
+
+    @Override
+    public void createFront() {
+        System.out.println("Front start!");
+    }
+}
+
+class ImplBackEnd implements BackEnder {
+
+    @Override
+    public void createServer() {
+        System.out.println("Drop server");
+    }
+}
+
+interface Developer{
+}
